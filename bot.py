d957aa7924bc6efd2b0a2f70e5076c05991704a7
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-01-31 20:42:45 +0300

    Запустил асинхронный код. Теперь причесываю

--- bot.py
+++ bot.py
@@ -1,22 +1,21 @@
-import asyncio
-import os
 import telegram
 
 
-last_lesson = {
-    "submitted_at":  lesson_review["new_attempts"][0]["submitted_at"],
-    "is_negative": lesson_review["new_attempts"][0]["is_negative"],
-    "lesson_title":  lesson_review["new_attempts"][0]["lesson_title"],
-    "lesson_url": lesson_review["new_attempts"][0]["lesson_url"]
-    }
+async def preparing_message(lesson_review):
+    if lesson_review["check_status"]:
+        result = "Преподавателю понравилось, можно приступать к следующему уроку"
+    else:
+        result = "К сожалению в работе нашлись ошибки"
+    massage = (
+        f'У Вас проверили работу!\n\n'
+        f'Урок: "{lesson_review["lesson_title"]}"\n'
+        f'Ссылка на урок: {lesson_review["lesson_url"]}\n'
+        f'Результат проверки: {result}'
+    )
+    return massage
 
 
-
-massage_accept = "Преподавателю понравилось, можно приступать к следующему уроку"
-massage_task_no_accepted = "К сожалению в работе нашлись ошибки"
-massage_task_checked = "У Вас проверили работу <<Отправляем уведомление о проверке работ>>"
-
-
-async def send_message():
-    await bot.send_message(text="Привет Дима", chat_id=chat_id)
-
+async def send_message(tg_token, chat_id, lesson_review):
+    message = await preparing_message(lesson_review)
+    bot = telegram.Bot(token=tg_token)
+    await bot.send_message(text=message, chat_id=chat_id)
--- main.py
+++ main.py
@@ -1,37 +1,41 @@
-import requests
 import os
-from bot import send_message
 import asyncio
-import os
-import telegram
 import time
+import requests
+from bot import send_message
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-def take_lesson_review(url_long, headers, params):
+async def prepare_lesson_review(lesson_review_data):
+    lesson_review = {
+        "submitted_at":  lesson_review_data["new_attempts"][0]["submitted_at"],
+        "check_status": lesson_review_data["new_attempts"][0]["is_negative"],
+        "lesson_title":  lesson_review_data["new_attempts"][0]["lesson_title"],
+        "lesson_url": lesson_review_data["new_attempts"][0]["lesson_url"]
+        }
+    return lesson_review
+
+
+async def take_lesson_review_data(url_long, headers, params):
     response = requests.get(url_long, headers=headers,  params=params, timeout=30)
     response = response.json()
     params["timestamp"] = response["last_attempt_timestamp"]
-    return response.json()
+    return response
 
 
-def main():
+async def main():
     tg_token = os.environ["TG_TOKEN"]
     chat_id = os.environ["CHAT_ID"]
     dvmn_token = os.environ["DVMN_TOKEN"]
-    bot = telegram.Bot(token=tg_token)
-    
     url_long = 'https://dvmn.org/api/long_polling/'
     headers = {"Authorization": f'Token {dvmn_token}'}
     params = {"timestamp": '0'}
-   
     connect_try = 1
     max_connect_try = 5
- 
     while True:
         try:
-            lesson_review = take_lesson_review(url_long, headers, params)
+            lesson_review_data = await take_lesson_review_data(url_long, headers, params)
         except requests.exceptions.Timeout:
             print("Timeout occurred")
             continue
@@ -44,8 +48,9 @@ def main():
             connect_try += 1
             continue
         connect_try = 1
-        # asyncio.run(send_message())
+        lesson_review = await prepare_lesson_review(lesson_review_data)
+        await send_message(tg_token, chat_id, lesson_review)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
