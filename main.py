feb51c94b5b9d61334bfda4465305641860db1ee
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-02-05 21:05:31 +0300

    ВЫнес тайм стемпт

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ async def take_lesson_review_data(
         async with session.get(url_long, headers=headers, params=params) as response:
             response.raise_for_status()
             response = await response.json()
-            params["timestamp"] = response["last_attempt_timestamp"]
             return response
 
 
@@ -43,6 +42,7 @@ async def main() -> None:
                 headers,
                 params
                 )
+            params["timestamp"] = lesson_review_data["last_attempt_timestamp"]
             await send_message(tg_token, chat_id, lesson_review_data)
         except requests.exceptions.ReadTimeout:
             continue
