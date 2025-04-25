6e1b5fc64bbdfe9e75300c1bd444e44d02ff1859
Шведов Дмитрий <shvedovds@kamaz.ru>
2025-04-25 11:33:37 +0300

    Добавил логгирование

--- main.py
+++ main.py
@@ -8,30 +8,18 @@ from dotenv import load_dotenv, find_dotenv
 from bot import send_message
 
 
+FORMAT = "%(asctime)s %(process)d %(levelname)s %(message)s"
+
+logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('Logger')
 logger.setLevel(logging.INFO)
 
-logging.debug('Сообщение для дебагинга')
-logging.info('Произошло какое-то событие. Всё идёт по плану.')
-logging.warning('Предупреждение, что-то могло сломаться')
-logging.error('Ошибка, что-то сломалось')
-logging.critical('МЫ В ОГНЕ ЧТО ДЕЛАТЬ?!?!')
 
-
-
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('Сообщение уровня DEBUG')
-logging.basicConfig(format="%(process)d %(levelname)s %(message)s")
-logging.warning("This is a Warning")
-
-class TelegramLogsHandler(logging.Handler):
-    Logging.handlers
-    
 async def take_lesson_review_data(
     url_long: str,
     headers: dict,
     params: dict
-    ) -> dict:
+) -> dict:
     '''Make a Api reuest
     params:
         url_long: url
@@ -41,19 +29,23 @@ async def take_lesson_review_data(
         json
     '''
     async with aiohttp.ClientSession() as session:
-        async with session.get(url_long, headers=headers, params=params) as response:
+        async with session.get(
+            url_long, headers=headers, params=params
+        ) as response:
             response.raise_for_status()
             response = await response.json()
             return response
 
 
 async def main() -> None:
+    logger.info("Программа стартует")
     load_dotenv(find_dotenv())
     tg_token = os.environ["TG_TOKEN"]
     chat_id = os.environ["CHAT_ID"]
     dvmn_token = os.environ["DVMN_TOKEN"]
     url_long = 'https://dvmn.org/api/long_polling/'
     headers = {"Authorization": f'Token {dvmn_token}'}
+    logger.info("Программа загрузила переменные")
     while True:
         params = {"timestamp": str()}
         try:
@@ -65,9 +57,13 @@ async def main() -> None:
             params["timestamp"] = lesson_review_data["last_attempt_timestamp"]
             await send_message(tg_token, chat_id, lesson_review_data)
         except requests.exceptions.ReadTimeout:
+            logging.info('ReadTimeout')
             continue
         except requests.exceptions.ConnectionError:
-            print("ConnectionError occurred")
+            logging.warning('ConnectionError occurred')
+            sleep(60)
+        except Exception:
+            logger.exception('BotFail')
             sleep(60)
 
 
